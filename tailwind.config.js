c8498503033ddad34118ba1a62cbdcc78d339d1a
DataCodeLiteracy <dataliteracy@icloud.com>
2023-05-03 02:45:11 +0900

    feat: footer 꾸미기

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        footerBg: "#1a2936"
+      }
+    }
   },
   plugins: []
 };
